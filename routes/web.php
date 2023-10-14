c83c21475753aeb52255cc3eee37461121951fca
Ignacio Lopez <o.lopezpinto@gmail.com>
2023-10-14 00:22:31 -0300

    Controllers e Views, Intro

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SobreNosController;
+use App\Http\Controllers\ContatoController;
+use App\Http\Controllers\PrincipalController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,12 +20,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-*/
+
 
 Route::get('/', function () {
     return 'Olá, seja bem-vindo!';
 });
-
 Route::get('/sobre-nos', function () {
     return 'Sobre nós';
 });
@@ -30,3 +32,24 @@ Route::get('/sobre-nos', function () {
 Route::get('/contato', function () {
     return 'Contato';
 });
+*/
+
+Route::get('/',[PrincipalController::class , 'principal']);
+Route::get('/sobre-nos', [SobreNosController::class , 'sobreNos']);
+Route::get('/contato', [ContatoController::class, 'contato']);
+
+// Passando parametros
+
+Route::get(
+    '/contato/{nome}/{categoria}/{assunto}/{mensagem}',
+    function(string $nome, string $categoria, string $assunto, string $mensagem) {
+
+    echo "<h1>Olá $nome</h3><br/>";
+    echo "<h5>Informações do Contato</h5><br/>";
+    echo "Categoria: $categoria<br/>";
+    echo "Assunto: $assunto<br/>";
+    echo "Mensagem: $mensagem";
+
+    }
+);
+
